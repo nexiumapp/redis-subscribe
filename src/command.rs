cc14babb3a773a9b0cd47c35d5c88651923ba392
0/0 <zero@imaskeleton.me>
2021-12-15 15:50:41 -0700

    add pattern commands to `Command` object

--- src/command.rs
+++ src/command.rs
@@ -2,6 +2,8 @@
 pub enum Command {
     Subscribe(String),
     Unsubscribe(String),
+    PatternSubscribe(String),
+    PatternUnsubscribe(String),
 }
 
 impl Command {
@@ -9,6 +11,8 @@ impl Command {
         match self {
             Command::Subscribe(channel) => format!("SUBSCRIBE {}\r\n", channel),
             Command::Unsubscribe(channel) => format!("UNSUBSCRIBE {}\r\n", channel),
+            Command::PatternSubscribe(channel) => format!("PSUBSCRIBE {}\r\n", channel),
+            Command::PatternUnsubscribe(channel) => format!("PUNSUBSCRIBE {}\r\n", channel),
         }
     }
 }
